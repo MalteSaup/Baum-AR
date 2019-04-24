18b3dc849a73b9ecf2ee3b9f8dbf8b1d94d9bc5e
malte.saupe@haw-hamburg.de <malte.saupe@haw-hamburg.de>
2019-04-24 11:35:30 +0200

    Präsi Zusammenwurf, ungetestet, unsauber

--- Baum AR/Baum AR/Assets/CubeScript.cs
+++ Baum AR/Baum AR/Assets/CubeScript.cs
@@ -7,20 +7,30 @@ public class CubeScript : MonoBehaviour
     // Start is called before the first frame update
 	Renderer c_renderer;
 	float angle;
-    	void Start(){
-		c_renderer = GetComponent<Renderer>();
-    	}
+	GameObject blaetter;
+    void Start(){
+		this.blaetter = (GameObject)transform.Find("blaetter").gameObject;
+		Debug.Log(blaetter);
+		c_renderer = this.blaetter.GetComponent<Renderer>();
+    }
 
 	// Update is called once per frame
     	void Update(){
-		Debug.Log(transform.eulerAngles.y);
-		angle = transform.eulerAngles.y * Mathf.PI / 180.0f;
-		if(angle != 90 && angle != 180){
-			c_renderer.material.color = new Color(Mathf.Cos(angle), Mathf.Sin(angle), 0.5f);
-		}
-		else{
-			c_renderer.material.color = new Color(Mathf.Cos(angle), Mathf.Sin(angle), 0.5f);	
-		}
+			Debug.Log(transform.eulerAngles.y);
+			angle = transform.eulerAngles.y * 3.14f / 180.0f;
+			if(angle >= 0 && angle < 90){   //Frühling
+				c_renderer.material.color = new Color(2 * Mathf.Abs(Mathf.Cos(angle)), 255 - 49 * Mathf.Abs(Mathf.Cos(angle)), 0, 255);
+			}
+			if(angle >= 90 && angle < 180){   //Sommer
+				c_renderer.material.color = new Color(2 + 204 * Mathf.Abs(Mathf.Sin(angle)), 206 - 85 * Mathf.Abs(Mathf.Sin(angle)), 2 * Mathf.Abs(Mathf.Sin(angle)), 255);
+			}
+			if(angle >= 180 && angle < 270){   //Herbst
+				c_renderer.material.color = new Color(206, 121, 2, 255 * Mathf.Abs(Mathf.Sin(angle)));
+			}
+			if(angle >= 270 && angle < 360){   //Winter
+				c_renderer.material.color = new Color(0, 255, 80, 255 * Mathf.Abs(Mathf.Cos(angle)));
+			}
+
     	}
 }
 //						Color.HSVtoRGB(transform.eulerAngles.y / 360.0f, 0.5f, 0.5f));
